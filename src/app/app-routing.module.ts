d190b07576684ce26fb40cfe9a373c4117f68b5f
AndresYusty <andrescardoso1002@gmail.com>
2022-11-29 20:26:56 -0500

    rutas de componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GestionarEnfermedadesComponent } from './gestionar-enfermedades/gestionar-enfermedades.component';
+import { GestionarEstudiantesComponent } from './gestionar-estudiantes/gestionar-estudiantes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: GestionarEstudiantesComponent},
+  {path: 'gestionar-estudiantes', component: GestionarEstudiantesComponent},
+  {path: 'gestionar-enfermedades', component: GestionarEnfermedadesComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
